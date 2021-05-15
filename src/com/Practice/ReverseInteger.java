65cb2b18fb5a1966f1bb054e2966ceb55268b74b
Kushagra Sharma <kushagra.sharma131@gmail.com>
2021-05-15 01:27:14 -0400

    Removed unused func()

--- src/com/Practice/ReverseInteger.java
+++ src/com/Practice/ReverseInteger.java
@@ -3,60 +3,6 @@ package com.Practice;
 import java.util.Arrays;
 
 public class ReverseInteger {
-	
-	public int reverse(int x) {
-		String str = String.valueOf(x);
-		
-		Object[] array = new Object[str.length()];
-		int[] intArray = new int[str.length()];
-		char[] chArray = new char[str.length()];
-        System.out.println("Num -> " + str);
-        
-        int quotient = Math.abs(x);
-        int i = 0;
-        int digit;
-        
-        if(x < 0){
-        	array[0] = (Object)str.charAt(0);
-        	chArray[0] = (char) array[0];
-        	i = 1;
-        }
-        
-        while(i < array.length){
-            
-            System.out.println("I -> " + i);
-            
-            digit = quotient % 10;
-            System.out.println("Digit -> " + digit);
-            
-            array[i] = digit;
-            System.out.println("Array -> " + Arrays.toString(array));
-            
-            intArray[i] = Integer.parseInt(String.valueOf(array[i]));
-            System.out.println("Int Array -> " + Arrays.toString(intArray));
-            
-            chArray[i] = (char)(intArray[i] + '0');
-            System.out.println("Temp -> " + Arrays.toString(chArray));
-            
-            quotient = quotient / 10;
-            System.out.println("Quotient -> " + quotient);
-            i++;
-        }
-        System.out.println("Reverse Array -> " + Arrays.toString(intArray));
-        String reverseNum = new String(chArray);
-        System.out.println("Reverse String -> " + reverseNum);
-        int reverse;
-        
-        try{
-        	reverse = Integer.parseInt(reverseNum);
-        }
-        catch(Exception e){
-        	System.out.println(e.getMessage());
-        	return 0;
-        }
-        
-        return reverse;
-    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
