fa99829673a6236a0ffbac121f74b9db83e91279
Kushagra Sharma <kushagra.sharma131@gmail.com>
2026-02-12 11:00:54 -0500

    Added Interview Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
-import org.example.coding.NumberOfIslands;
+import org.example.coding.Interview;
+
+import java.util.List;
 
 public class Main {
 
@@ -8,6 +10,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        NumberOfIslands.main(null);
+        Interview.main(List.of("checkPattern").toArray(new String[0]));
     }
 }
--- src/main/java/org/example/coding/Interview.java
+++ src/main/java/org/example/coding/Interview.java
@@ -1,7 +1,43 @@
 package org.example.coding;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class Interview {
 
-    public static void main(String[] args) {  }
+    public static void main(String[] args) {
+        if ("checkPattern".equals(args[0])) {
+            checkPattern();
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
 
+    private static void checkPattern() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of test cases");
+        int number = Integer.parseInt(sc.nextLine());
+        while (number > 0) {
+            System.out.println("Enter the regex pattern");
+            String pattern = sc.nextLine();
+            // --> Code Logic here
+            try {
+                if (isValidPattern(pattern)) System.out.println("Valid");
+                else System.out.println("Invalid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            number--;
+        }
+        sc.close();
+    }
+
+    private static boolean isValidPattern(String regex) throws PatternSyntaxException {
+        // Edge case
+        if (regex == null || regex.isEmpty()) return false;
+
+        Pattern pattern = Pattern.compile(regex);
+        return true;
+    }
 }
